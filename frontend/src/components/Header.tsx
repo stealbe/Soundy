8663826a58eab8086c897ffa94b921d90c2818bf
Smile <Maksimgem2009max@gmail.com>
2026-06-12 11:20:05 +0300

    Update Header component styles and add ChevronDown SVG

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({user = null}: {user: {avatar_url: string} | null
 
   return (
 
-    <header className="main-container w-[100%] h-[155px] relative overflow-hidden mx-auto my-0">
+    <header className="main-container w-[100%] h-[155px] relative overflow-visible mx-auto my-0">
 
       {/* Semi-transparent overlay that preserves required background color */}
       <div
@@ -28,11 +28,9 @@ export default function Header({user = null}: {user: {avatar_url: string} | null
         style={{ background: 'rgba(18, 18, 18, 0.75)' }}
       />
       {/* Search box with LiquidGL effect */}
-      <div className="liquidGL absolute w-[474px] h-[59px] bg-[#2d2d2d] rounded-[15px] top-[48px] left-[35%] z-[12] flex items-center px-4">
-        
-      </div>
-      <div className="w-[55px] h-[34.041px] bg-[url(https://static.codia.ai/image/2026-06-11/nT2DmxQU5E.png)] bg-cover bg-no-repeat absolute top-[85px] left-[94%] z-[9]" />
-      <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#fff] absolute top-[85px] left-[27%] text-left whitespace-nowrap z-[15]">
+      <div className="main-container w-[474px] h-[59px] bg-[rgba(82,81,81,0.17)] rounded-[15px] left-[50%] -translate-x-1/2 opacity-75 relative mx-auto my-0" />
+      <div className="w-[55px] h-[34.041px] bg-[url(https://static.codia.ai/image/2026-06-11/nT2DmxQU5E.png)] bg-cover bg-no-repeat absolute top-[75px] left-[94%] z-[9]" />
+      <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#fff] absolute top-[85px] left-[26%] text-left whitespace-nowrap z-[15]">
         Library
       </span>
 
@@ -43,7 +41,7 @@ export default function Header({user = null}: {user: {avatar_url: string} | null
       <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#fff] absolute top-[85px] left-[13%] text-left whitespace-nowrap z-[15]">
         Home
       </span>
-      <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#fff] absolute top-[85px] left-[19%] text-left whitespace-nowrap z-[15]">
+      <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#fff] absolute top-[85px] left-[20%] text-left whitespace-nowrap z-[15]">
         Feed
       </span>
       <span className="flex h-[22px] justify-start items-start font-sans text-[18px] font-bold leading-[21.784px] text-[#ff9603] absolute top-[85px] left-[58%] text-left whitespace-nowrap z-[15]">
@@ -55,13 +53,13 @@ export default function Header({user = null}: {user: {avatar_url: string} | null
 
       {/* Icons */}
       <div className="w-[36px] h-[45px] bg-[url(https://static.codia.ai/image/2026-06-11/Ke2Rvob85z.png)] bg-cover bg-no-repeat absolute top-[70px] left-[53%] z-[12]" />
-      <div className="w-[36px] h-[45px] bg-[url(https://static.codia.ai/image/2026-06-11/qvPrmcAHmB.png)] bg-contain bg-no-repeat opacity-[0.19] absolute top-[70px] left-[87%] z-[12]" />
-      <div className="w-[36px] h-[45px] bg-[url(https://static.codia.ai/image/2026-06-11/QDKm2pJ1Wi.png)] bg-contain bg-no-repeat opacity-[0.19] absolute top-[70px] left-[91%] z-[12]" />
+      <div className="w-[36px] h-[45px] bg-[url(https://static.codia.ai/image/2026-06-11/qvPrmcAHmB.png)] bg-contain bg-no-repeat opacity-[0.19] absolute top-[70px] left-[86%] z-[12]" />
+      <div className="w-[36px] h-[45px] bg-[url(https://static.codia.ai/image/2026-06-11/QDKm2pJ1Wi.png)] bg-contain bg-no-repeat opacity-[0.19] absolute top-[70px] left-[90%] z-[12]" />
+      <div className="w-[70px] h-[65px] bg-[url(/ChevronDown.svg)] bg-contain bg-no-repeat absolute top-[80px] left-[82%] z-[12]" />
       
       {/* User avatar */}
-      <Image width={59} height={59} className="rounded-full w-[59px] h-[59px] absolute top-[48px] left-[79%] z-[13]" src={user?.avatar_url || '/no-avatar.webp'} alt="User Avatar" />
+      <Image width={59} height={59} className="rounded-full w-[59px] h-[59px] absolute top-[65px] left-[79%] z-[13]" src={user?.avatar_url || '/no-avatar.webp'} alt="User Avatar" />
     </header>
   );
-
-
 };
+
